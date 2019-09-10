c071be1a1e21a4c9188fc32cc182f852d7db3148
Iñaki Diez <inakidiezgalarza@gmail.com>
2019-09-10 17:07:42 -0300

    Agregadas colisiones y reset de bola

--- PongSigillib/pongSigil.cpp
+++ PongSigillib/pongSigil.cpp
@@ -1,62 +1,110 @@
 #include "sl.h"
 #include <iostream>
-struct Paleta
+//SETEAR EN X86 PARA QUE FUNCIONE
+struct Jugador
 {
 	float x;
 	float y=300;
 	float altura=100;
 	float ancho=10;
-	bool enPantalla = true;
+	int Puntaje = 0;
 };
+bool Colisionando = false;
+bool Colisionando2 = false;
 int BolaRadio = 10;
 float BolaX = 300;
 float BolaY = 200;
 float BolaMovX = 4;
 float BolaMovY = 4;
-Paleta P1;
-Paleta P2;
+Jugador P1;
+Jugador P2;
 int PantallaAncho = 600;
 int PantallaAlto = 400;
 
 int main(int args, char *argv[])
 {
+	if (P1.Puntaje>=10||P2.Puntaje>=10)
+	{
+		slClose;
+	}
 	P1.x = 10;
 	P2.x = PantallaAncho - 10;
-	// set up our window and a few resources we need
 	slWindow(PantallaAncho, PantallaAlto, "Pong en sigil, masomeno", false);
 	double dt=slGetDeltaTime();
 	while (!slShouldClose() && !slGetKey(SL_KEY_ESCAPE))
 	{
-		BolaX += BolaMovX;
-		BolaY += BolaMovY;
-		if (BolaY+BolaRadio>=PantallaAlto)BolaMovY*=-1;
-		if (BolaY - BolaRadio <= 0)BolaMovY *= -1;
-		if (BolaX + BolaRadio >= P2.x&&BolaY<=P2.y+P2.altura/2&&BolaY>=P2.y-P2.altura)BolaMovX *= -1;//QUE NO SEA MAYOR O IGUAL SINO MAYOR AL PRINCIPIO DE LA X Y DE LA X MAS EL ANCHO
-		if (BolaX - BolaRadio <= P1.x)BolaMovX *= -1;
-		if (P1.y>=0&&P1.y<=PantallaAlto)
+		if (BolaX + BolaRadio >= P2.x&&BolaX - BolaRadio <= P2.x &&BolaY - BolaRadio <= P2.y + P2.altura / 2 && BolaY + BolaRadio >= P2.y - P2.altura / 2)
 		{
-			P1.enPantalla = true;
+			Colisionando = true;
 		}
 		else
 		{
-			P1.enPantalla = false;
+			Colisionando = false;
 		}
-		//input
+		if (BolaX - BolaRadio <= P1.x&&BolaX + BolaRadio >= P1.x &&BolaY - BolaRadio <= P1.y + P1.altura / 2 && BolaY + BolaRadio >= P1.y - P1.altura / 2)
+		{
+			Colisionando2 = true;
+		}
+		else
+		{
+			Colisionando2 = false;
+		}
+		BolaX += BolaMovX;
+		BolaY += BolaMovY;
+		if (slGetKey(SL_KEY_LEFT_CONTROL) == true)BolaMovY *= -1;
 		if (slGetKey(SL_KEY_DOWN)==true && P2.y - P2.altura / 2 > 0)P2.y-=5;
 		if (slGetKey(SL_KEY_UP)==true && P2.y+P2.altura/2 < PantallaAlto)P2.y+=5;
 		if (slGetKey('S') == true&&P1.y-P1.altura/2>0)P1.y -= 5;
 		if (slGetKey('W') == true && P1.y + P1.altura / 2 < PantallaAlto)P1.y += 5;
-		// large text and fat line
+		if (BolaY+BolaRadio>=PantallaAlto)BolaMovY*=-1;
+		if (BolaY - BolaRadio <= 0)BolaMovY *= -1;
+		if (BolaX + BolaRadio >= P2.x&&BolaX - BolaRadio <= P2.x &&BolaY - BolaRadio <= P2.y + P2.altura / 2 && BolaY + BolaRadio >= P2.y - P2.altura / 2 &&Colisionando == false)
+		{
+			if (BolaY < P2.y&&BolaMovY>0)
+			{
+				BolaMovY *= -1.0f;
+			}
+			if (BolaY > P2.y&&BolaMovY<0)
+			{
+				BolaMovY *= -1.0f;
+			}
+			BolaMovX *= -1;
+			//Colisionando = true;//innecesario creo
+		}
+		if (BolaX - BolaRadio <= P1.x&&BolaX + BolaRadio >= P1.x &&BolaY - BolaRadio <= P1.y + P1.altura / 2 && BolaY + BolaRadio >= P1.y - P1.altura / 2 && Colisionando2 == false)
+		{
+			if (BolaY < P1.y&&BolaMovY>0)
+			{
+				BolaMovY *= -1.0f;
+			}
+			if (BolaY > P1.y&&BolaMovY < 0)
+			{
+				BolaMovY *= -1.0f;
+			}
+			BolaMovX *= -1;
+			//Colisionando2 = true;//innecesario creo
+		}
+		if (BolaX + BolaRadio >= PantallaAncho)
+		{
+			BolaMovX *= -1;
+			BolaX = P2.x-10;
+			BolaY = P2.y;
+			P1.Puntaje += 1;
+		}
+		if (BolaX - BolaRadio <= 0)
+		{
+			BolaMovX *= -1;
+			BolaX = P1.x + 10;
+			BolaY = P1.y;
+			P2.Puntaje += 1;
+		}
+		
 		slSetForeColor(0.0, 0.8, 0.2, 1.0);
 		slRectangleFill(P1.x, P1.y, P1.ancho, P1.altura);
 		slRectangleFill(P2.x, P2.y, P2.ancho, P2.altura);
 		slCircleFill(BolaX, BolaY, BolaRadio, 15);
-		// smaller subtext
-		slSetFontSize(14);
-		slText(200, 220, "Sprites, text, sound, input, and more!");
-		slLine(300, 400, 300, 0);
-		slRender();// draw everything
+		slRender();
 	}
-	slClose();// close the window and shut down SIGIL
+	slClose();
 	return 0;
 }
